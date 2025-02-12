e5fc263aa50ffb4d62e000a339ef674665dea486
nsy2k <newskyy2k@gmail.com>
2025-02-12 22:35:26 +0700

    Rm git cache

--- Server/DAL/DataModels/PasswordSet.cs
+++
@@ -1,9 +0,0 @@
-namespace DAL.DataModels
-{
-    [Serializable]
-    public class PasswordSet(byte[] pwdHash, byte[] pwdSalt)
-    {
-        public byte[] PwdHash { get; set; } = pwdHash;
-        public byte[] PwdSalt { get; set; } = pwdSalt;
-    }
-}
